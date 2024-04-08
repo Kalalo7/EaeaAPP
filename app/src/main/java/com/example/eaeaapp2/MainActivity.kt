c1b18ed9a1d6d319ab8b4150c45de3f1940e2453
Kalalo7 <martinfauci@gmail.com>
2024-04-08 21:03:54 +0200

    app terminada funcional

---
+++ app/src/main/java/com/example/eaeaapp2/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.eaeaapp2
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.eaeaapp2.ui.theme.Eaeaapp2Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Eaeaapp2Theme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BotonCentrado()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BotonCentrado() {
+    val openDialog = remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        val backgroundImage = painterResource(R.drawable.background_image)
+        val dialogImage = painterResource(R.drawable.dialog_image)
+        Image(
+            painter = backgroundImage,
+            contentDescription = "fondo",
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Button(onClick = {
+            openDialog.value = true
+            MediaPlayer.create(context, R.raw.sound_file).start()
+        }) {
+            Text(text = "EaeAPP")
+        }
+        if (openDialog.value) {
+            AlertDialog(
+                onDismissRequest = { openDialog.value = false },
+                title = { Text(text = "EAEAAPP") },
+                text = { Image(painter = dialogImage, contentDescription = null) },
+                confirmButton = {
+                    TextButton(onClick = { openDialog.value = false }) {
+                        Text(text = "OK")
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Eaeaapp2Theme {
+        BotonCentrado()
+    }
+}
